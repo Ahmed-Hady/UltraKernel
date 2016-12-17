e6806ac7862a823e91ee6c2d613895d0bac2762f
Ahmed Hady <ahmedhady6@gmail.com>
2016-12-18 01:37:25 +0200

    clean app tree 2

--- app/src/main/java/com/ultrakernel/Main.java
+++ app/src/main/java/com/ultrakernel/Main.java
@@ -80,13 +80,8 @@ public class Main extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_main) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
 
