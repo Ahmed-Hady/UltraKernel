19a9fdc473fd6cc251429c62274edf27537261a6
Ahmed Hady <ahmedhady6@gmail.com>
2016-12-18 15:04:25 +0200

    Adding Memory Monitor

--- app/src/main/java/com/ultrakernel/fragment/SystemInfo_fragement.java
+++ app/src/main/java/com/ultrakernel/fragment/SystemInfo_fragement.java
@@ -1,13 +1,19 @@
 package com.ultrakernel.fragment;
 
+import android.app.ActivityManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.ultrakernel.R;
+
+import java.text.DecimalFormat;
+
+import static android.content.Context.ACTIVITY_SERVICE;
 
 public class SystemInfo_fragement  extends Fragment {
     @Nullable
@@ -15,7 +21,53 @@ public class SystemInfo_fragement  extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragement_systeminfo, container, false);
+
+        ActivityManager activityManager = (ActivityManager) this.getContext().getSystemService(ACTIVITY_SERVICE);
+        ActivityManager.MemoryInfo memoryInfo = new ActivityManager.MemoryInfo();
+        activityManager.getMemoryInfo(memoryInfo);
+
+        TextView total_ram = (TextView) view.findViewById(R.id.t_ram);
+        TextView free_ram = (TextView) view.findViewById(R.id.f_ram);
+        TextView used_ram = (TextView) view.findViewById(R.id.u_ram);
+        TextView ram_perc = (TextView) view.findViewById(R.id.u_ram_perc);
+
+        total_ram.setText(" " + size((int) memoryInfo.totalMem));
+        used_ram.setText(" " + size((int) (memoryInfo.totalMem-memoryInfo.availMem)));
+        free_ram.setText(" " + size((int) memoryInfo.availMem));
+
+        double uRam = (memoryInfo.totalMem-memoryInfo.availMem);
+        double rRam = (uRam / memoryInfo.totalMem)*100;
+        DecimalFormat dec = new DecimalFormat("0");
+        ram_perc.setText("" + dec.format(rRam).concat("%"));
+
         return view;
     }
 
+    public String size(int size){
+        String hrSize = "";
+        int k = size;
+        double m = size/1024;
+        double g = size/1048576;
+
+        DecimalFormat dec = new DecimalFormat("0.0");
+
+        if (k>0)
+        {
+
+            hrSize = dec.format(k).concat(" KB");
+
+        }
+        if (m>0)
+        {
+
+            hrSize = dec.format(m).concat(" MB");
+        }
+        if (g>0)
+        {
+
+            hrSize = dec.format(g/1000).concat(" GB");
+        }
+
+        return hrSize;
+    }
 }
