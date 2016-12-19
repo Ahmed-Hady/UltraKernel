3e9b617191da68d508b87e94fd04d45061fae436
Ahmed Hady <ahmedhady6@gmail.com>
2016-12-19 23:57:37 +0200

    Add Battery Service

--- app/src/main/java/com/ultrakernel/fragment/SystemInfo_fragement.java
+++ app/src/main/java/com/ultrakernel/fragment/SystemInfo_fragement.java
@@ -1,6 +1,9 @@
 package com.ultrakernel.fragment;
 
 import android.app.ActivityManager;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -29,9 +32,10 @@ public class SystemInfo_fragement  extends Fragment implements SwipeRefreshLayou
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         final View view = inflater.inflate(R.layout.fragement_systeminfo, container, false);
-        ActivityManager activityManager = (ActivityManager) this.getContext().getSystemService(ACTIVITY_SERVICE);
 
         // RAM Monitor
+
+        ActivityManager activityManager = (ActivityManager) this.getContext().getSystemService(ACTIVITY_SERVICE);
         ActivityManager.MemoryInfo memoryInfo = new ActivityManager.MemoryInfo();
         activityManager.getMemoryInfo(memoryInfo);
 
@@ -85,6 +89,13 @@ public class SystemInfo_fragement  extends Fragment implements SwipeRefreshLayou
             TextView kernel=(TextView) view.findViewById(R.id.kernel);
             List<String> d_kernel= Shell.SH.run("cat /proc/version");
             kernel.setText("" + d_kernel);
+
+        // Battery
+
+        TextView B=(TextView) view.findViewById(R.id.battery);
+        B.setText(readBattery());
+
+
         // Refresh
         swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipeContainer);
         swipeRefreshLayout.setOnRefreshListener(this);
@@ -148,5 +159,87 @@ public class SystemInfo_fragement  extends Fragment implements SwipeRefreshLayou
         //when your data has finished loading, cset the refresh state of the view to false
         swipeRefreshLayout.setRefreshing(false);
 
+    }
+
+
+
+    private String readBattery(){
+        StringBuilder sb = new StringBuilder();
+        IntentFilter batteryIntentFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryIntent = getActivity().registerReceiver(null, batteryIntentFilter);
+
+        boolean  present= batteryIntent.getExtras().getBoolean(BatteryManager.EXTRA_PRESENT);
+        sb.append("PRESENT: " + present + "\n");
+
+        int status = batteryIntent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+        if(status == BatteryManager.BATTERY_STATUS_CHARGING){
+            sb.append("BATTERY_STATUS_CHARGING\n");
+        }
+        if(status == BatteryManager.BATTERY_STATUS_FULL){
+            sb.append("BATTERY_STATUS_FULL\n");
+        }
+
+        int plugged = batteryIntent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+        if(plugged == BatteryManager.BATTERY_PLUGGED_USB){
+            sb.append("BATTERY_PLUGGED_USB\n");
+        }
+        if(plugged == BatteryManager.BATTERY_PLUGGED_AC){
+            sb.append("BATTERY_PLUGGED_AC\n");
+        }
+
+        int level = batteryIntent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        sb.append("LEVEL: " + level + "\n");
+
+        int scale = batteryIntent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+        sb.append("SCALE: " + scale + "\n");
+
+        int  health = batteryIntent.getIntExtra(BatteryManager.EXTRA_HEALTH,0);
+        sb.append("health: " + convHealth(health) + "\n");
+
+        String  technology = batteryIntent.getExtras().getString(BatteryManager.EXTRA_TECHNOLOGY);
+        sb.append("TECHNOLOGY: " + technology + "\n");
+
+        int  temperature = batteryIntent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE,0);
+        sb.append("TEMPERATURE: " + temperature + "\n");
+
+        int  voltage = batteryIntent.getIntExtra(BatteryManager.EXTRA_VOLTAGE,0);
+        sb.append("VOLTAGE: " + voltage + "\n");
+
+        //I have no idea how to load the small icon from system resources!
+        int  icon_small_resourceId = batteryIntent.getIntExtra(BatteryManager.EXTRA_ICON_SMALL,0);
+        sb.append("ICON_SMALL: " + icon_small_resourceId + "\n");
+
+        return sb.toString();
+    }
+
+    private String convHealth(int health){
+        String result;
+        switch(health){
+            case BatteryManager.BATTERY_HEALTH_COLD:
+                result = "BATTERY_HEALTH_COLD";
+                break;
+            case BatteryManager.BATTERY_HEALTH_DEAD:
+                result = "BATTERY_HEALTH_DEAD";
+                break;
+            case BatteryManager.BATTERY_HEALTH_GOOD:
+                result = "BATTERY_HEALTH_GOOD";
+                break;
+            case BatteryManager.BATTERY_HEALTH_OVERHEAT:
+                result = "BATTERY_HEALTH_OVERHEAT";
+                break;
+            case BatteryManager.BATTERY_HEALTH_OVER_VOLTAGE:
+                result = "BATTERY_HEALTH_OVER_VOLTAGE";
+                break;
+            case BatteryManager.BATTERY_HEALTH_UNKNOWN:
+                result = "BATTERY_HEALTH_UNKNOWN";
+                break;
+            case BatteryManager.BATTERY_HEALTH_UNSPECIFIED_FAILURE:
+                result = "BATTERY_HEALTH_UNSPECIFIED_FAILURE";
+                break;
+            default:
+                result = "unkknown";
+        }
+
+        return result;
     }
 }
