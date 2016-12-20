2192cd0ade0785407e494622b833952829509e70
Ahmed Hady <ahmedhady6@gmail.com>
2016-12-20 02:04:49 +0200

    floating button

--- app/src/main/java/com/ultrakernel/Main.java
+++ app/src/main/java/com/ultrakernel/Main.java
@@ -1,9 +1,7 @@
 package com.ultrakernel;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
@@ -31,15 +29,6 @@ public class Main extends AppCompatActivity
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         /*Fragments*/
         mMain=new Main_fragement();
