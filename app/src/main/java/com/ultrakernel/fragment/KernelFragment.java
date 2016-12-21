27bd6c74b2baf13da7c57dff81c2b3983398bade
Ahmed Hady <ahmedhady6@gmail.com>
2016-12-21 05:50:44 +0200

    Add Kernel features boxes

--- app/src/main/java/com/ultrakernel/fragment/KernelFragment.java
+++ app/src/main/java/com/ultrakernel/fragment/KernelFragment.java
@@ -6,14 +6,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.ultrakernel.R;
+
+import static com.ultrakernel.util.Config.Android_d_kernel;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class KernelFragment extends Fragment {
 
+    private TextView kerne_info;
 
     public KernelFragment() {
         // Required empty public constructor
@@ -24,7 +28,11 @@ public class KernelFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_kernel, container, false);
+        final View view = inflater.inflate(R.layout.fragment_kernel, container, false);
+
+        kerne_info = (TextView) view.findViewById(R.id.kInfo);
+        kerne_info.setText("" + Android_d_kernel());
+        return view;
     }
 
 }
