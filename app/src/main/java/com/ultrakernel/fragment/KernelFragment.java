165fd352f4f050c1f65629ac8dccb8229b5e7c3d
Ahmed Hady <ahmedhady6@gmail.com>
2016-12-27 14:43:32 +0200

    Revert "Governor: add tmp buttton for governor chooser"
    This reverts commit 1b32620dc75acaf7b1b50b298df74975633124ff.

--- app/src/main/java/com/ultrakernel/fragment/KernelFragment.java
+++ app/src/main/java/com/ultrakernel/fragment/KernelFragment.java
@@ -6,7 +6,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.ultrakernel.R;
@@ -19,7 +18,11 @@ import static com.ultrakernel.util.Config.Android_d_kernel;
 public class KernelFragment extends Fragment {
 
     private TextView kerne_info;
-    private Button GovernorButton;
+
+    public KernelFragment() {
+        // Required empty public constructor
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -28,15 +31,6 @@ public class KernelFragment extends Fragment {
         final View view = inflater.inflate(R.layout.fragment_kernel, container, false);
 
         kerne_info = (TextView) view.findViewById(R.id.kInfo);
-        GovernorButton=(Button)view.findViewById(R.id.tmp_button);
-        GovernorButton.setText("Choose Governor");
-        GovernorButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                GovernorOptionDialogFragment fragment = new GovernorOptionDialogFragment();
-                fragment.show(getActivity().getSupportFragmentManager(), "power_dialog_fragment");
-            }
-        });
         kerne_info.setText("" + Android_d_kernel());
         return view;
     }
