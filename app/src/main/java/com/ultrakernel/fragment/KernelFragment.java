287e661df911a3ed9f7b2941fac4328c1a04462f
Ahmed Hady <ahmedhady6@gmail.com>
2016-12-29 01:33:22 +0200

    Check if the device supports d2w

--- app/src/main/java/com/ultrakernel/fragment/KernelFragment.java
+++ app/src/main/java/com/ultrakernel/fragment/KernelFragment.java
@@ -6,11 +6,26 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.ultrakernel.R;
 
+import java.io.File;
+
+import static com.ultrakernel.util.Config.ANDROID_TOUCH2_DT2W;
+import static com.ultrakernel.util.Config.ANDROID_TOUCH_DT2W;
 import static com.ultrakernel.util.Config.Android_d_kernel;
+import static com.ultrakernel.util.Config.DT2W_ENABLE;
+import static com.ultrakernel.util.Config.DT2W_FT5X06;
+import static com.ultrakernel.util.Config.DT2W_WAKEUP_GESTURE;
+import static com.ultrakernel.util.Config.DT2W_WAKE_GESTURE;
+import static com.ultrakernel.util.Config.DT2W_WAKE_GESTURE_2;
+import static com.ultrakernel.util.Config.LGE_TOUCH_CORE_DT2W;
+import static com.ultrakernel.util.Config.LGE_TOUCH_DT2W;
+import static com.ultrakernel.util.Config.LGE_TOUCH_GESTURE;
+import static com.ultrakernel.util.Config.TOUCH_PANEL_DT2W;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +38,7 @@ public class KernelFragment extends Fragment {
         // Required empty public constructor
     }
 
+    private LinearLayout d2w;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -32,7 +48,36 @@ public class KernelFragment extends Fragment {
 
         kerne_info = (TextView) view.findViewById(R.id.kInfo);
         kerne_info.setText("" + Android_d_kernel());
-        return view;
+
+        d2w=(LinearLayout)view.findViewById(R.id.d2w);
+
+        if(new File(LGE_TOUCH_DT2W).exists()){
+            d2w.setVisibility(RelativeLayout.VISIBLE);
+        }else if(new File(LGE_TOUCH_CORE_DT2W).exists()){
+            d2w.setVisibility(RelativeLayout.VISIBLE);
+        }else if(new File(LGE_TOUCH_GESTURE).exists()){
+            d2w.setVisibility(RelativeLayout.VISIBLE);
+        }else if(new File(ANDROID_TOUCH_DT2W).exists()) {
+            d2w.setVisibility(RelativeLayout.VISIBLE);
+        }else if(new File(ANDROID_TOUCH2_DT2W).exists()){
+            d2w.setVisibility(RelativeLayout.VISIBLE);
+        } else if(new File(TOUCH_PANEL_DT2W).exists()){
+            d2w.setVisibility(RelativeLayout.VISIBLE);
+        }else if(new File(DT2W_WAKEUP_GESTURE).exists()){
+            d2w.setVisibility(RelativeLayout.VISIBLE);
+        } else if(new File(DT2W_ENABLE).exists()){
+            d2w.setVisibility(RelativeLayout.VISIBLE);
+        }else if(new File(DT2W_WAKE_GESTURE).exists()){
+            d2w.setVisibility(RelativeLayout.VISIBLE);
+        }else if(new File(DT2W_WAKE_GESTURE_2).exists()){
+            d2w.setVisibility(RelativeLayout.VISIBLE);
+        } else if(new File(DT2W_FT5X06).exists()){
+            d2w.setVisibility(RelativeLayout.VISIBLE);
+        }else{
+            d2w.setVisibility(RelativeLayout.GONE);
+        }
+
+    return view;
     }
 
 }
--- app/src/main/java/com/ultrakernel/util/Config.java
+++ app/src/main/java/com/ultrakernel/util/Config.java
@@ -37,4 +37,16 @@ public class Config {
         return mShell.runAsRoot();
     }
 
+    public static final String LGE_TOUCH_DT2W = "/sys/devices/virtual/input/lge_touch/dt_wake_enabled";
+    public static final String LGE_TOUCH_CORE_DT2W = "/sys/module/lge_touch_core/parameters/doubletap_to_wake";
+    public static final String LGE_TOUCH_GESTURE = "/sys/devices/virtual/input/lge_touch/touch_gesture";
+    public static final String ANDROID_TOUCH_DT2W = "/sys/android_touch/doubletap2wake";
+    public static final String ANDROID_TOUCH2_DT2W = "/sys/android_touch2/doubletap2wake";
+    public static final String TOUCH_PANEL_DT2W = "/proc/touchpanel/double_tap_enable";
+    public static final String DT2W_WAKEUP_GESTURE = "/sys/devices/virtual/input/input1/wakeup_gesture";
+    public static final String DT2W_ENABLE = "/sys/devices/platform/s3c2440-i2c.3/i2c-3/3-004a/dt2w_enable";
+    public static final String DT2W_WAKE_GESTURE = "/sys/devices/platform/spi-tegra114.2/spi_master/spi2/spi2.0/input/input0/wake_gesture";
+    public static final String DT2W_WAKE_GESTURE_2 = "/sys/devices/soc.0/f9924000.i2c/i2c-2/2-0070/input/input0/wake_gesture";
+    public static final String DT2W_FT5X06 = "/sys/bus/i2c/drivers/ft5x06_i2c/5-0038/d2w_switch";
+
 }
