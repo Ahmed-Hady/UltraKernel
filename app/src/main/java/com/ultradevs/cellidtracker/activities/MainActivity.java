8e1953a2fd3925f7d47af05988f1989f6307a82f
Ahmed Hady <ahmedhady6@gmail.com>
2017-09-20 00:14:00 +0200

    Regenerating the app

--- app/src/main/java/com/ultradevs/cellidtracker/activities/MainActivity.java
+++ app/src/main/java/com/ultradevs/cellidtracker/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.yshalsager.cellidtracker;
+package com.ultradevs.cellidtracker.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.ultradevs.cellidtracker.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
