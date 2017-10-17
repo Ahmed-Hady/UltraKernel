5378542b407ad4150df104ed3b786f40ae0fb994
Ahmed Hady <ahmedhady6@gmail.com>
2017-10-17 19:57:54 +0200

    SystemInfo: Add Android System Information

--- app/src/main/java/com/ultradevs/ultrakernel/fragments/SystemInfoFragment.java
+++ app/src/main/java/com/ultradevs/ultrakernel/fragments/SystemInfoFragment.java
@@ -1,32 +1,74 @@
 package com.ultradevs.ultrakernel.fragments;
 
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.ultradevs.ultrakernel.R;
+import com.ultradevs.ultrakernel.adapters.BatteryStatusAdapter;
+import com.ultradevs.ultrakernel.adapters.bat_status_list;
+
+import java.util.ArrayList;
+
+import static com.ultradevs.ultrakernel.utils.SystemInfoUtils.Android_Name;
+import static com.ultradevs.ultrakernel.utils.SystemInfoUtils.Android_Sdk_Version;
+import static com.ultradevs.ultrakernel.utils.SystemInfoUtils.Android_Version;
+import static com.ultradevs.ultrakernel.utils.SystemInfoUtils.Android_device_board;
+import static com.ultradevs.ultrakernel.utils.SystemInfoUtils.Android_device_kernel;
+import static com.ultradevs.ultrakernel.utils.SystemInfoUtils.Android_device_manuf;
+import static com.ultradevs.ultrakernel.utils.SystemInfoUtils.Android_device_name;
+import static com.ultradevs.ultrakernel.utils.SystemInfoUtils.Android_system_patch_Version;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SystemInfoFragment extends Fragment {
 
+    ArrayList<bat_status_list> arrayOfSystem = new ArrayList<bat_status_list>();
+
+    public BatteryStatusAdapter adapter;
+
+    ListView sysinfolist;
 
     public SystemInfoFragment() {
         // Required empty public constructor
     }
 
-
+    @SuppressLint("ResourceType")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_sysinfo, container, false);
 
-        //ImageView img = v.findViewById(R.id.sysLogo);
+        getActivity().setTitle(R.string.sys_info_title);
+
+        adapter = new BatteryStatusAdapter(getContext(), arrayOfSystem);
+        sysinfolist = (ListView) v.findViewById(R.id.sys_info_list);
+        sysinfolist.setAdapter(adapter);
+
+        TextView os_name = v.findViewById(R.id.txt_os_name);
+        TextView os_version = v.findViewById(R.id.txt_os_version);
+        TextView os_sdk = v.findViewById(R.id.txt_os_sdk);
+
+        os_name.setText(Android_Name());
+        os_version.setText(Android_Version());
+        os_sdk.setText("Android SDK: " + Android_Sdk_Version());
+
+        adapter.add(new bat_status_list("Android Code Name", Android_Name()));
+        adapter.add(new bat_status_list("Android Version", Android_Version()));
+        adapter.add(new bat_status_list("SDK Version", Android_Sdk_Version()));
+        adapter.add(new bat_status_list("Security Patch", Android_system_patch_Version()));
+        adapter.add(new bat_status_list("Device Name", Android_device_name()));
+        adapter.add(new bat_status_list("Device SOC", Android_device_board()));
+        adapter.add(new bat_status_list("Device Manufacturer", Android_device_manuf()));
+        adapter.add(new bat_status_list("Kernel Version", Android_device_kernel()));
 
         return v;
     }
---
+++ app/src/main/java/com/ultradevs/ultrakernel/utils/SystemInfoUtils.java
@@ -0,0 +1,61 @@
+package com.ultradevs.ultrakernel.utils;
+
+import java.io.IOException;
+
+/**
+ * Created by ahmedhady on 17/10/17.
+ */
+
+public class SystemInfoUtils {
+
+    public static ShellExecuter mShell;
+
+    public static String Android_Version() {
+        mShell.command="getprop ro.build.version.release";
+        return mShell.runAsRoot();
+    }
+    public static String Android_Name() {
+        if (Android_Version().startsWith("4.4")){
+            return "Android KitKat";
+        }
+        if (Android_Version().startsWith("5")){
+            return "Android Lollipop";
+        }
+        if (Android_Version().startsWith("6")){
+            return "Android Marshmallow";
+        }
+        if (Android_Version().startsWith("7")){
+            return "Android Nougat";
+        }
+        if (Android_Version().startsWith("8")){
+            return "Android Oreo";
+        }
+        return null;
+    }
+    public static String Android_Sdk_Version(){
+        mShell.command="getprop ro.build.version.sdk";
+        return mShell.runAsRoot();
+    }
+    public static String Android_system_patch_Version(){
+        mShell.command="getprop ro.build.version.security_patch";
+        return mShell.runAsRoot();
+    }
+    public static String Android_device_board(){
+        mShell.command="getprop ro.product.board";
+        return mShell.runAsRoot();
+    }
+    public static String Android_device_manuf(){
+        mShell.command="getprop ro.product.manufacturer";
+        return mShell.runAsRoot();
+    }
+
+    public static String Android_device_name(){
+        mShell.command="getprop ro.product.model";
+        return mShell.runAsRoot();
+    }
+
+    public static String Android_device_kernel(){
+        mShell.command="cat /proc/version";
+        return mShell.runAsRoot();
+    }
+}
