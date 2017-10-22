0abf9909578b7b6a8b3800c4ebf815d9e776e392
Ahmed Hady <ahmedhady6@gmail.com>
2017-10-22 09:42:21 +0200

    ShellExecuter: Remove excessive line in runAsRoot Command

--- app/src/main/java/com/ultradevs/ultrakernel/utils/ShellExecuter.java
+++ app/src/main/java/com/ultradevs/ultrakernel/utils/ShellExecuter.java
@@ -10,6 +10,7 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
@@ -21,23 +22,24 @@ public class ShellExecuter {
     public static String command;
 
     public static final  String runAsRoot() {
-
         try {
-            Process process = Runtime.getRuntime().exec(command);
-            BufferedReader reader = new BufferedReader(
-                    new InputStreamReader(process.getInputStream()));
-            int read;
-            char[] buffer = new char[4096];
-            StringBuffer output = new StringBuffer();
-            while ((read = reader.read(buffer)) > 0) {
-                output.append(buffer, 0, read);
+            Process p = Runtime.getRuntime().exec(command);
+            InputStream is = null;
+            if (p.waitFor() == 0) {
+                is = p.getInputStream();
+            } else {
+                is = p.getErrorStream();
             }
-            reader.close();
-            return output.toString();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            BufferedReader br = new BufferedReader(new InputStreamReader(is),
+                    4096);
+            String line = br.readLine();
+            br.close();
+            return line;
+        } catch (Exception ex) {
+            return "ERROR: " + ex.getMessage();
         }
     }
+
     public static boolean hasSelinux()
     {
         return new File("/sys/fs/selinux/enforce").isFile();
