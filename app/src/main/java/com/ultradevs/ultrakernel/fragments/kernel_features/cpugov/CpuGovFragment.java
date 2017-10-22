239dcfc9fc45983ecf5fa3d6b3dc88cc323bec64
Ahmed Hady <ahmedhady6@gmail.com>
2017-10-22 14:14:05 +0200

    Initializing CPU GOV section

--- app/src/main/java/com/ultradevs/ultrakernel/fragments/kernel_features/cpugov/CpuGovFragment.java
+++ app/src/main/java/com/ultradevs/ultrakernel/fragments/kernel_features/cpugov/CpuGovFragment.java
@@ -6,19 +6,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.ultradevs.ultrakernel.R;
+
+import static com.ultradevs.ultrakernel.fragments.deviceInfo.KernelInfoFragment.kernel_Current_Gov;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CpuGovFragment extends Fragment {
 
+    public TextView mCurrent;
 
     public CpuGovFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -28,6 +31,9 @@ public class CpuGovFragment extends Fragment {
 
         getActivity().setTitle(getString(R.string.k_cpu_gov));
 
+        mCurrent = (TextView) v.findViewById(R.id.currGov);
+
+        mCurrent.setText(kernel_Current_Gov());
         return v;
     }
 
