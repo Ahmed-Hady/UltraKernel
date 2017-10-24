8475e262a49af0dbff2678aa6876d3a9add4be3c
Ahmed Hady <ahmedhady6@gmail.com>
2017-10-24 22:40:09 +0200

    OnBoot: Set OnBoot Notification

--- app/src/main/java/com/ultradevs/ultrakernel/services/OnBootApply.java
+++ app/src/main/java/com/ultradevs/ultrakernel/services/OnBootApply.java
@@ -1,8 +1,13 @@
 package com.ultradevs.ultrakernel.services;
 
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import static android.R.attr.id;
+
+import com.ultradevs.ultrakernel.R;
 
 public class OnBootApply extends Service {
     public OnBootApply() {
@@ -24,6 +29,18 @@ public class OnBootApply extends Service {
     }
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+
+
+        final NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setContentTitle("UltraKernel")
+                        .setContentText("Hello World !");
+
+        final NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(this.NOTIFICATION_SERVICE);
+
+        mNotificationManager.notify(id, mBuilder.build());
+
         return Service.START_NOT_STICKY;
     }
 }
